8138ad939760bce23e0b78a7512cf22b6d410b76
rakibtg <rakibtg@gmail.com>
2018-04-22 02:39:08 +0600

    header animation added

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,18 @@ window.onload = function () {
     hljs.highlightBlock( element )
   }
 }
+
+window.onscroll = function ( e ) {
+  if ( document.documentElement.scrollTop > 0 ) {
+    this.document.getElementById( 'header' ).classList.remove( 'undoActAnimationOnHeader' )
+    if ( this.document.getElementById( 'header' ).classList[1] != 'actAnimationOnHeader' ) {
+      this.document.getElementById( 'header' ).classList.add( 'actAnimationOnHeader' )
+      this.document.getElementById( 'header' ).classList.add( 'actAnimationOnHeader' )
+    }
+  } else if ( document.documentElement.scrollTop == 0 ) {
+    if ( this.document.getElementById( 'header' ).classList[1] == 'actAnimationOnHeader' ) {
+      this.document.getElementById( 'header' ).classList.remove( 'actAnimationOnHeader' )
+      this.document.getElementById( 'header' ).classList.add( 'undoActAnimationOnHeader' )
+    }
+  }
+}
