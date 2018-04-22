359c430bd30b0def5523297098a7252fd32555cc
rakibtg <rakibtg@gmail.com>
2018-04-22 19:19:00 +0600

    almost done docs

--- assets/app.js
+++ assets/app.js
@@ -28,16 +28,19 @@ function handleDocNav(event) {
 }
 
 window.onscroll = function ( e ) {
-  if ( document.documentElement.scrollTop > 0 ) {
-    this.document.getElementById( 'header' ).classList.remove( 'undoActAnimationOnHeader' )
-    if ( this.document.getElementById( 'header' ).classList[1] != 'actAnimationOnHeader' ) {
-      this.document.getElementById( 'header' ).classList.add( 'actAnimationOnHeader' )
-      this.document.getElementById( 'header' ).classList.add( 'actAnimationOnHeader' )
-    }
-  } else if ( document.documentElement.scrollTop == 0 ) {
-    if ( this.document.getElementById( 'header' ).classList[1] == 'actAnimationOnHeader' ) {
-      this.document.getElementById( 'header' ).classList.remove( 'actAnimationOnHeader' )
-      this.document.getElementById( 'header' ).classList.add( 'undoActAnimationOnHeader' )
+  var windowWidth = document.body["scrollWidth"]
+  if ( windowWidth > 800 ) {
+    if ( document.documentElement.scrollTop > 0 ) {
+      this.document.getElementById( 'header' ).classList.remove( 'undoActAnimationOnHeader' )
+      if ( this.document.getElementById( 'header' ).classList[1] != 'actAnimationOnHeader' ) {
+        this.document.getElementById( 'header' ).classList.add( 'actAnimationOnHeader' )
+        this.document.getElementById( 'header' ).classList.add( 'actAnimationOnHeader' )
+      }
+    } else if ( document.documentElement.scrollTop == 0 ) {
+      if ( this.document.getElementById( 'header' ).classList[1] == 'actAnimationOnHeader' ) {
+        this.document.getElementById( 'header' ).classList.remove( 'actAnimationOnHeader' )
+        this.document.getElementById( 'header' ).classList.add( 'undoActAnimationOnHeader' )
+      }
     }
   }
 }
